57904aca46b3e000add87b4245f3af78e996b048
lurts <47759111+lurts@users.noreply.github.com>
2024-11-27 04:13:53 +0100

    zoom update

--- README.md
+++ README.md
@@ -3,8 +3,8 @@
 ![Mandelbrot](https://github.com/lurts/P2A8_Mandelbrot/blob/master/pictures/mandelbrot-1.png)
 
 - multithreaded
+- zooming via mousewheel
 - uses a logarithmic scale to increase contrast and detail
 
 ## TODO:
-- implement zoom (my past attempts did not agree with multithreading so I chose performance over features)
 - experiment with different scales and add a way to switch scales
--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include "mandelbrot.h"
 
 const bool multithreading = true;
+float zoomFactor = 1.5f;
 
 /* --Vereinfachte Schreibweise. */
 using Complex = std::complex<double>;
@@ -31,17 +32,24 @@ int main() {
     // viel aus foren und mit hilfe von chatGPT geschrieben
     unsigned int numThreads;
     if (multithreading) {
+        // maximale anzahl möglicher threads vom system abfragen
         numThreads = std::thread::hardware_concurrency();
     }
     else {
         numThreads = 1;
     }
     
+    // um zugriffsfehler zu vermeiden muss der bereich auf die threads aufgeteilt werden
+    // weil alle threads schreiben auf das selbe VertexArray
     unsigned int rowsPerThread = window_height / numThreads;
+
     std::vector<std::thread> threads;
 
     for (unsigned int t = 0; t < numThreads; ++t) {
+        // start und endpunkt für neuen thread bestimmen
         unsigned int startY = t * rowsPerThread;
+        // letzter thread bekommt als endwert einfach "window_height"
+        // wird gebraucht falls "window_height" nicht genau durch die anzahl der threads teilbar ist
         unsigned int endY = (t == numThreads - 1) ? window_height : startY + rowsPerThread;
 
         threads.emplace_back(&Mandelbrot::calculatePartial, &MB, std::ref(VA), std::ref(area), startY, endY, 255);
@@ -63,6 +71,47 @@ int main() {
             {
                 if (event.key.code == sf::Keyboard::Escape) {
                     window.close();
+                }
+            }
+
+            if (event.type == sf::Event::MouseWheelScrolled) {
+                // zoom richtung und maus position bestimmen
+                float zoomFactorDirectional = (event.mouseWheelScroll.delta > 0)
+                    ? 1.0f / zoomFactor
+                    : zoomFactor;
+                sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(window));
+
+                // umrechnung auf komplexe koordinaten
+                Complex zoomCenter = {
+                    c0.real() + (c1.real() - c0.real()) * (mousePos.x / window_width),
+                    c0.imag() + (c1.imag() - c0.imag()) * (mousePos.y / window_height)
+                };
+
+                double newWidth = (c1.real() - c0.real()) * zoomFactorDirectional;
+                double newHeight = (c1.imag() - c0.imag()) * zoomFactorDirectional;
+
+                // Setze neuen Bereich
+                c0 = Complex(zoomCenter.real() - newWidth / 2.0, zoomCenter.imag() - newHeight / 2.0);
+                c1 = Complex(zoomCenter.real() + newWidth / 2.0, zoomCenter.imag() + newHeight / 2.0);
+
+                MB.set(c0, c1);
+
+                // threads neu starten
+                for (auto& thread : threads) {
+                    if (thread.joinable()) {
+                        thread.join();  // Warten, bis der Thread beendet ist
+                    }
+                }
+                threads.clear();  // Jetzt ist es sicher, die Liste zu leeren
+
+                for (unsigned int t = 0; t < numThreads; ++t) {
+                    // start und endpunkt für neuen thread bestimmen
+                    unsigned int startY = t * rowsPerThread;
+                    // letzter thread bekommt als endwert einfach "window_height"
+                    // wird gebraucht falls "window_height" nicht genau durch die anzahl der threads teilbar ist
+                    unsigned int endY = (t == numThreads - 1) ? window_height : startY + rowsPerThread;
+
+                    threads.emplace_back(&Mandelbrot::calculatePartial, &MB, std::ref(VA), std::ref(area), startY, endY, 255);
                 }
             }
         }
